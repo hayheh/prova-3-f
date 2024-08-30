fd109edaec76c8c30783da34b91a8ce3fd03288c
hayheh <127457351+hayheh@users.noreply.github.com>
2024-08-29 22:33:39 -0300

    Update main.js
    2908

---
+++ main.js
@@ -0,0 +1,19 @@
+ function sum(number1, number2) {
+    console.log(number1 + number2)
+ }
+ sum(1000,1000)
+
+
+
+-
+
+ var name = 'Alunos do 3 ano F'
+
+ console.log(name);
+
+
+
+-
+
+
+alert('olá Mundo')
